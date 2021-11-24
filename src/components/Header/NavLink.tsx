9e33507c64866124f8d71a26cfaf6784a72df91e
Hugo <hugolacerdar@gmail.com>
2021-11-24 19:04:30 -0300

    Add Navigation Link component

---
+++ src/components/Header/NavLink.tsx
@@ -0,0 +1,27 @@
+import {
+  Link as ChakraLink,
+  Text,
+  LinkProps as ChakraLinkProps,
+} from "@chakra-ui/react";
+import { ActiveLink } from "./ActiveLink";
+
+interface NavLinkProps extends ChakraLinkProps {
+  children: string;
+  href: string;
+}
+
+export default function NavLink({ children, href, ...rest }: NavLinkProps) {
+  return (
+    <ActiveLink href={href} passHref shouldMatchExactHref={true}>
+      <ChakraLink
+        display="flex"
+        align="center"
+        {...rest}
+        _focus={{ outline: "none" }}
+        _hover={{ textDecor: "none" }}
+      >
+        <Text fontWeight="medium">{children}</Text>
+      </ChakraLink>
+    </ActiveLink>
+  );
+}
