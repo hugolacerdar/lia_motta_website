05796f27ce6e4a10b288978333d54abeb979629d
Hugo <hugolacerdar@gmail.com>
2021-11-26 20:16:13 -0300

    Add Drawer Context

---
+++ src/contexts/DrawerContext.tsx
@@ -0,0 +1,29 @@
+import { useDisclosure, UseDisclosureReturn } from "@chakra-ui/hooks";
+import { useRouter } from "next/router";
+import { createContext, ReactNode, useContext, useEffect } from "react";
+
+interface DrawerProviderProps {
+  children: ReactNode;
+}
+
+type DrawerContextData = UseDisclosureReturn;
+
+const DrawerContext = createContext({} as DrawerContextData);
+
+export function DrawerProvider({ children }: DrawerProviderProps) {
+  const disclosure = useDisclosure();
+  const router = useRouter();
+
+  useEffect(() => {
+    disclosure.onClose();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [router.asPath]);
+
+  return (
+    <DrawerContext.Provider value={disclosure}>
+      {children}
+    </DrawerContext.Provider>
+  );
+}
+
+export const useDrawer = () => useContext(DrawerContext);
