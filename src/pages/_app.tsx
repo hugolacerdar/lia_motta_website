749721f926044054a6ffeb9c2888acb5d95fd8cf
Hugo <hugolacerdar@gmail.com>
2021-11-26 20:16:20 -0300

    Add Drawer Context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,20 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
+import Footer from "../components/Footer";
 import Header from "../components/Header";
+import SideDrawer from "../components/Header/Drawer";
+import { DrawerProvider } from "../contexts/DrawerContext";
 import { theme } from "../styles/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <Header />
-      <Component {...pageProps} />
+      <DrawerProvider>
+        <Header />
+        <SideDrawer />
+        <Component {...pageProps} />
+        <Footer />
+      </DrawerProvider>
     </ChakraProvider>
   );
 }
