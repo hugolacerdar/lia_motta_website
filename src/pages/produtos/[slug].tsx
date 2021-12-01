3c45d689be511782bcb798bb03cc64b0de8d201b
Hugo <hugolacerdar@gmail.com>
2021-12-01 16:24:10 -0300

    Add conditional display for free products

--- src/pages/produtos/[slug].tsx
+++ src/pages/produtos/[slug].tsx
@@ -1,5 +1,8 @@
 import { Image } from "@chakra-ui/image";
 import {
+  Input,
+  InputGroup,
+  InputLeftElement,
   Container,
   Flex,
   Icon,
@@ -19,7 +22,11 @@ import {
 } from "pure-react-carousel";
 import Link from "next/link";
 import Head from "next/head";
-import { RiArrowLeftSLine, RiArrowRightSLine } from "react-icons/ri";
+import {
+  RiArrowLeftSLine,
+  RiArrowRightSLine,
+  RiMailSendLine,
+} from "react-icons/ri";
 
 import "pure-react-carousel/dist/react-carousel.es.css";
 import { ParsedUrlQuery } from "querystring";
@@ -64,37 +71,37 @@ export default function SingleProductPage({ product }: SingleProductPageProps) {
         <Link href="/produtos">Produtos</Link> <Icon as={RiArrowRightSLine} />{" "}
         {product.title}{" "}
       </Flex>
-      <Grid gridTemplateColumns={["1fr", "1fr", "1fr", "1fr 2fr"]} gap="30px">
-        <Container>
+      <Grid gridTemplateColumns={["1fr", "1fr", "1fr", "1.5fr 2fr"]} gap="30px">
+        <Container maxW={["320px", "350px", "400px", "400px"]}>
           <CarouselProvider
             naturalSlideWidth={1080}
             naturalSlideHeight={1350}
             totalSlides={3}
           >
-            <Slider style={{ "max-height": "420px" }}>
+            <Slider>
               <Slide index={0}>
                 <Image
                   src={product.images[0].url}
                   alt={product.images[0].alt}
-                  h={400}
+                  w={375}
                 />
               </Slide>
               <Slide index={1}>
                 <Image
                   src={product.images[1].url}
                   alt={product.images[0].alt}
-                  h={400}
+                  w={375}
                 />
               </Slide>
               <Slide index={2}>
                 <Image
                   src={product.images[2].url}
                   alt={product.images[0].alt}
-                  h={400}
+                  w={375}
                 />
               </Slide>
             </Slider>
-            <Flex justifyContent="center" width="320px">
+            <Flex justifyContent="center" width="300px" mt="10px">
               <ButtonBack>
                 <Icon as={RiArrowLeftSLine} w={8} h={8} mr={10} />
               </ButtonBack>
@@ -114,24 +121,93 @@ export default function SingleProductPage({ product }: SingleProductPageProps) {
             dangerouslySetInnerHTML={{ __html: product.description }}
           />
 
-          <Link href={product.stripeUrl} passHref>
-            <Button
+          {product.price > 0 ? (
+            <Link href={product.stripeUrl} passHref>
+              <Button
+                mt="30px"
+                color="white"
+                bgColor="gray.900"
+                _hover={{
+                  bgColor: "white",
+                  color: "gray.900",
+                  borderColor: "gray.900",
+                  border: "1px",
+                }}
+                _focus={{ outline: "none" }}
+                padding="35px"
+                w="100%"
+              >
+                COMPRAR AGORA
+              </Button>
+            </Link>
+          ) : (
+            <Grid
+              gridTemplateColumns={["1fr", "1fr", "1fr", "1fr", "1.5fr 1fr"]}
               mt="30px"
-              color="white"
-              bgColor="gray.900"
-              _hover={{
-                bgColor: "white",
-                color: "gray.900",
-                borderColor: "gray.900",
-                border: "1px",
-              }}
-              _focus={{ outline: "none" }}
-              padding="35px"
-              w="100%"
             >
-              COMPRAR AGORA
-            </Button>
-          </Link>
+              <InputGroup>
+                <InputLeftElement pointerEvents="none">
+                  <Icon as={RiMailSendLine} color="gray.900" />
+                </InputLeftElement>
+                <Input
+                  borderColor="gray.900"
+                  borderTopRightRadius={[
+                    "var(--chakra-radii-md)",
+                    "var(--chakra-radii-md)",
+                    "var(--chakra-radii-md)",
+                    "var(--chakra-radii-md)",
+                    "0",
+                  ]}
+                  borderBottomRightRadius="0"
+                  borderBottomLeftRadius={[
+                    "0",
+                    "0",
+                    "0",
+                    "0",
+                    "var(--chakra-radii-md)",
+                  ]}
+                  borderRight={[
+                    "solid 1px gray.900",
+                    "solid 1px gray.900",
+                    "solid 1px gray.900",
+                    "solid 1px gray.900",
+                    "none",
+                  ]}
+                  _focus={{ outline: "none", backgroundColor: "gray.100" }}
+                  _hover={{ backgroundColor: "gray.100" }}
+                />
+              </InputGroup>
+              <Button
+                borderBottomLeftRadius={[
+                  "var(--chakra-radii-md)",
+                  "var(--chakra-radii-md)",
+                  "var(--chakra-radii-md)",
+                  "var(--chakra-radii-md)",
+                  "0",
+                ]}
+                borderTopLeftRadius="0"
+                borderTopRightRadius={[
+                  "0",
+                  "0",
+                  "0",
+                  "0",
+                  "var(--chakra-radii-md)",
+                ]}
+                color="white"
+                bgColor="gray.900"
+                _hover={{
+                  bgColor: "white",
+                  color: "gray.900",
+                  borderColor: "gray.900",
+                  border: "1px",
+                }}
+                _focus={{ outline: "none" }}
+                w="100%"
+              >
+                RECEBER WORKBOOK
+              </Button>
+            </Grid>
+          )}
         </Box>
       </Grid>
     </Box>
