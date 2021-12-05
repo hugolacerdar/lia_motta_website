906e0bc21dd0f35a0766e0e6f92d6b67d40ef25d
Hugo <hugolacerdar@gmail.com>
2021-12-05 13:47:23 -0300

    Add Pix test section

--- src/pages/produtos/[slug].tsx
+++ src/pages/produtos/[slug].tsx
@@ -10,6 +10,16 @@ import {
   Box,
   Heading,
   Button,
+  Accordion,
+  AccordionButton,
+  AccordionItem,
+  AccordionPanel,
+  AccordionIcon,
+  Text,
+  FormControl,
+  FormLabel,
+  Switch,
+  useClipboard,
 } from "@chakra-ui/react";
 import { GetServerSideProps } from "next";
 import { RichText } from "prismic-dom";
@@ -25,13 +35,18 @@ import Head from "next/head";
 import {
   RiArrowLeftSLine,
   RiArrowRightSLine,
+  RiCheckLine,
+  RiFileCopyLine,
   RiMailSendLine,
+  RiQrCodeFill,
 } from "react-icons/ri";
 
 import "pure-react-carousel/dist/react-carousel.es.css";
 import { ParsedUrlQuery } from "querystring";
 
 import { getPrismicClient } from "../../services/prismic";
+import formatMoney from "../../utils/formatMoney";
+import { useState } from "react";
 
 interface Params extends ParsedUrlQuery {
   slug: string;
@@ -46,6 +61,7 @@ interface Product {
   description: string;
   price: number;
   stripeUrl: string;
+  qrCode: { imageUrl: string; code: string };
   slug: string;
   updatedAt: Date;
   images: Image[];
@@ -56,6 +72,10 @@ interface SingleProductPageProps {
 }
 
 export default function SingleProductPage({ product }: SingleProductPageProps) {
+  const [showQRCode, setShowQRCode] = useState(false);
+
+  const { hasCopied, onCopy } = useClipboard(product.qrCode?.code);
+
   return (
     <Box maxW={["100vw", "90vw", "90vw", "70vw"]} mx="auto">
       <Head>
@@ -116,9 +136,14 @@ export default function SingleProductPage({ product }: SingleProductPageProps) {
           </CarouselProvider>
         </Container>
         <Box mx={["10px", "10px", "10px", "0"]}>
-          <Heading size="lg" mb="20px">
-            {product.title}
-          </Heading>
+          <Flex justifyContent="space-between">
+            <Heading size="lg" mb="20px">
+              {product.title}
+            </Heading>
+            <Heading size="lg" fontWeight="regular">
+              {formatMoney(product.price)}
+            </Heading>
+          </Flex>
           <Box
             as="div"
             ml={["10px", "10px", "10px", "0"]}
@@ -126,24 +151,134 @@ export default function SingleProductPage({ product }: SingleProductPageProps) {
           />
 
           {product.price > 0 ? (
-            <Link href={product.stripeUrl} passHref>
-              <Button
-                mt="30px"
-                color="white"
-                bgColor="gray.900"
-                _hover={{
-                  bgColor: "white",
-                  color: "gray.900",
-                  borderColor: "gray.900",
-                  border: "1px",
-                }}
-                _focus={{ outline: "none" }}
-                padding="35px"
-                w="100%"
-              >
-                COMPRAR AGORA
-              </Button>
-            </Link>
+            <>
+              <Link href={product.stripeUrl} passHref>
+                <Button
+                  mt="30px"
+                  color="white"
+                  bgColor="gray.900"
+                  _hover={{
+                    bgColor: "white",
+                    color: "gray.900",
+                    borderColor: "gray.900",
+                    border: "1px",
+                  }}
+                  _focus={{ outline: "none" }}
+                  padding="35px"
+                  w="100%"
+                >
+                  COMPRAR AGORA
+                </Button>
+              </Link>
+              <Accordion allowMultiple>
+                <AccordionItem>
+                  <h2>
+                    <AccordionButton
+                      borderRadius="var(--chakra-radii-md)"
+                      mt="20px"
+                      color="white"
+                      bgColor="gray.900"
+                      _hover={{
+                        bgColor: "white",
+                        color: "gray.900",
+                        borderColor: "gray.900",
+                        border: "1px",
+                      }}
+                      _focus={{ outline: "none" }}
+                      padding="25px"
+                      w="100%"
+                    >
+                      <Box flex="1" textAlign="center" fontWeight="bold">
+                        COMPRAR VIA PIX - 5% de desconto
+                      </Box>
+                      <AccordionIcon />
+                    </AccordionButton>
+                  </h2>
+                  <AccordionPanel pb={4}>
+                    <Text>
+                      De:{" "}
+                      <Text as="span" textDecor="line-through">
+                        {formatMoney(product.price)}
+                      </Text>
+                    </Text>
+                    <Text>
+                      por:{" "}
+                      <Text as="span" fontWeight="bold">
+                        {formatMoney(product.price * 0.95)}
+                      </Text>
+                      <Text mt="20px" textAlign="justify">
+                        No momento, estamos enviando manualmente o e-book por
+                        e-mail. Logo, solicitamos o envio do comprovante de
+                        pagamento para o endereço de e-mail{" "}
+                        <Text as="span" fontWeight="bold">
+                          contato@liamotta.com
+                        </Text>
+                        .
+                      </Text>
+                      <FormControl display="flex" alignItems="center" mt="10px">
+                        <FormLabel htmlFor="mail-condition" mb="0">
+                          <Text fontWeight="bold">
+                            Concordo com as condições acima:
+                          </Text>
+                        </FormLabel>
+                        <Switch
+                          id="mail-condition"
+                          colorMode="light"
+                          onChange={(e) => setShowQRCode(e.target.checked)}
+                        />
+                      </FormControl>
+                    </Text>
+                  </AccordionPanel>
+                </AccordionItem>
+              </Accordion>
+              {showQRCode ? (
+                <Box>
+                  <Image
+                    src={product.qrCode.imageUrl}
+                    alt={product.qrCode.code}
+                    mx="auto"
+                    w="320px"
+                  />
+                  <InputGroup w="320px" mx="auto">
+                    <InputLeftElement pointerEvents="none">
+                      <Icon as={RiQrCodeFill} color="gray.900" />
+                    </InputLeftElement>
+                    <Input
+                      borderColor="gray.900"
+                      _focus={{
+                        outline: "none",
+                        backgroundColor: "gray.100",
+                      }}
+                      _hover={{ backgroundColor: "gray.100" }}
+                      value={product.qrCode.code}
+                      isTruncated
+                      borderTopRightRadius="0"
+                      borderBottomRightRadius="0"
+                      borderRight="none"
+                    />
+                    <Button
+                      onClick={onCopy}
+                      color="white"
+                      bgColor="gray.900"
+                      _hover={{
+                        bgColor: "gray.700",
+                      }}
+                      _focus={{ outline: "none" }}
+                      borderTopLeftRadius="0"
+                      borderBottomLeftRadius="0"
+                    >
+                      {hasCopied ? (
+                        <Icon as={RiCheckLine} color="white" />
+                      ) : (
+                        <Icon as={RiFileCopyLine} color="white" />
+                      )}
+                    </Button>
+                  </InputGroup>
+                </Box>
+              ) : (
+                ""
+              )}
+            </>
           ) : (
             <Grid
               gridTemplateColumns={["1fr", "1fr", "1fr", "1fr", "1.5fr 1fr"]}
@@ -234,6 +369,10 @@ export const getServerSideProps: GetServerSideProps = async ({
     description: RichText.asHtml(response.data.descricao),
     price: response.data.preco,
     stripeUrl: response.data.pagamento.url,
+    qrCode: {
+      imageUrl: response.data.qrcode.url,
+      code: response.data.qrcode.alt,
+    },
     updatedAt: new Date(
       response.last_publication_date as string
     ).toLocaleDateString("pt-BR", {
