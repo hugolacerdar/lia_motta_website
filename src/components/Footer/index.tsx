020a5ec517138958c7d41d56f29fb5c065c9dddd
Hugo <hugolacerdar@gmail.com>
2021-12-07 21:08:58 -0300

    Add home page offset

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,5 +1,15 @@
 import { Box } from "@chakra-ui/layout";
+import { useRouter } from "next/router";
 
 export default function Footer() {
-  return <Box h="100px"></Box>;
+  const { asPath } = useRouter();
+
+  const isHome = asPath === "/";
+
+  return (
+    <>
+      {isHome ? <Box h="100vh" w="0" /> : ""}
+      <Box h="200px" pos="relative"></Box>
+    </>
+  );
 }
