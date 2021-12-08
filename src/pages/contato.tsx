5118f74e8eb0b9900c4991ab5568720c676cb916
Hugo <hugolacerdar@gmail.com>
2021-12-07 21:09:28 -0300

    Add path

--- src/pages/contato.tsx
+++ src/pages/contato.tsx
@@ -1,12 +1,26 @@
 import Head from "next/head";
+import { Box, Grid, Image, Text, Stack, Flex, Icon } from "@chakra-ui/react";
+import { RiArrowRightSLine } from "react-icons/ri";
+import Link from "next/link";
 
 export default function ContactPage() {
   return (
-    <div>
+    <Box maxW={["100vw", "90vw", "90vw", "70vw"]} mx="auto" mt="20px">
       <Head>
         <title>Contato | Lia Motta</title>
       </Head>
-      Contact
-    </div>
+      <Head>
+        <title>Contato | Lia Motta</title>
+      </Head>
+      <Flex
+        alignItems="center"
+        color="gray.500"
+        marginBottom="30px"
+        ml={["10px", "10px", "10px", "0"]}
+      >
+        <Link href="/">Início</Link> <Icon as={RiArrowRightSLine} />{" "}
+        <Link href="/produtos">Contato</Link>
+      </Flex>
+    </Box>
   );
 }
