6b043a9c0c4b9af1ee3a910ade6b8820d25e8364
Hugo <hugolacerdar@gmail.com>
2021-12-09 13:06:35 -0300

    Update list mx

--- src/pages/termos.tsx
+++ src/pages/termos.tsx
@@ -1,20 +1,15 @@
 import Head from "next/head";
 import {
   Box,
-  Grid,
-  Image,
   Text,
-  Stack,
   Flex,
   Icon,
-  List,
   OrderedList,
   ListItem,
   Heading,
 } from "@chakra-ui/react";
 import { RiArrowRightSLine } from "react-icons/ri";
 import Link from "next/link";
-import ContactForm from "../components/ContactForm";
 
 export default function TermsPage() {
   return (
@@ -31,32 +26,34 @@ export default function TermsPage() {
         <Link href="/">Início</Link> <Icon as={RiArrowRightSLine} />{" "}
         <Link href="/produtos">Termos de Compra</Link>
       </Flex>
-      <Heading size="lg">Termos de Compra</Heading>
-      <Text fontSize="lg" my="20px">
-        Quando você faz uma compra neste site, você está de acordo com as
-        seguintes políticas:
-      </Text>
-      <OrderedList textAlign="justify" fontSize="lg">
-        <ListItem>
-          Todos os produtos são digitais, mas também podem ser utilizados
-          impressos, caso prefira;
-        </ListItem>
-        <ListItem>Não aceitamos devoluções;</ListItem>
-        <ListItem>Não aceitamos cancelamentos;</ListItem>
-        <ListItem>
-          Caso você tenha qualquer dificuldade em acessar sua compra mande um
-          email para contato@liamotta.com detalhando sua situação;
-        </ListItem>
-        <ListItem>
-          O envio dos produtos é realizado dentro de 24 horas a partir da
-          confirmação do pagamento, tanto para as compras com cartão, quanto
-          para as via PIX.
-        </ListItem>
-        <ListItem>
-          Caso você efetue sua compra via PIX, não se esqueça de enviar o
-          comprovante para contato@liamotta.com.
-        </ListItem>
-      </OrderedList>{" "}
+      <Box mx={["20px", "0", "0", "0"]}>
+        <Heading size="lg">Termos de Compra</Heading>
+        <Text fontSize="lg" my="20px">
+          Quando você faz uma compra neste site, você está de acordo com as
+          seguintes políticas:
+        </Text>
+        <OrderedList textAlign="justify" fontSize="lg">
+          <ListItem>
+            Todos os produtos são digitais, mas também podem ser utilizados
+            impressos, caso prefira;
+          </ListItem>
+          <ListItem>Não aceitamos devoluções;</ListItem>
+          <ListItem>Não aceitamos cancelamentos;</ListItem>
+          <ListItem>
+            Caso você tenha qualquer dificuldade em acessar sua compra mande um
+            email para contato@liamotta.com detalhando sua situação;
+          </ListItem>
+          <ListItem>
+            O envio dos produtos é realizado dentro de 24 horas a partir da
+            confirmação do pagamento, tanto para as compras com cartão, quanto
+            para as via PIX.
+          </ListItem>
+          <ListItem>
+            Caso você efetue sua compra via PIX, não se esqueça de enviar o
+            comprovante para contato@liamotta.com.
+          </ListItem>
+        </OrderedList>{" "}
+      </Box>
     </Box>
   );
 }
