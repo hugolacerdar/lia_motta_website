3a6d5eee6ef6a2f836de7efd460d6eab16518fc9
Hugo <hugolacerdar@gmail.com>
2021-12-12 18:06:30 -0300

    Delete unused import

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,19 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
+import Footer from "../components/Footer";
 import Header from "../components/Header";
 import SideDrawer from "../components/Header/Drawer";
 import { DrawerProvider } from "../contexts/DrawerContext";
-import Fonts from "../styles/Fonts";
 import { theme } from "../styles/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <Fonts />
       <DrawerProvider>
         <Header />
         <SideDrawer />
         <Component {...pageProps} />
+        <Footer />
       </DrawerProvider>
     </ChakraProvider>
   );
