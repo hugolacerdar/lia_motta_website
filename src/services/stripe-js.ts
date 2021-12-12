3241f6109e4ff098b66a65490e6c20a696394c0b
Hugo <hugolacerdar@gmail.com>
2021-12-12 18:06:00 -0300

    Delete unused

--- src/services/stripe-js.ts
+++
@@ -1,9 +0,0 @@
-import { loadStripe } from "@stripe/stripe-js";
-
-export async function getStripeJS() {
-  const stripeJS = await loadStripe(
-    process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY as string
-  );
-
-  return stripeJS;
-}
