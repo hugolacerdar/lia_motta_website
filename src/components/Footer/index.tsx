03b40fe0c94cd351688e9b6e4d0b16d97538d857
Hugo <hugolacerdar@gmail.com>
2021-12-12 18:07:51 -0300

    Add footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,8 +1,18 @@
 import { Box, Container } from "@chakra-ui/layout";
 import { useRouter } from "next/router";
-import { HStack, Link, Icon, Text, Flex } from "@chakra-ui/react";
+import {
+  HStack,
+  Center,
+  Link as ChakraLink,
+  Icon,
+  Text,
+  Flex,
+  Heading,
+  Grid,
+} from "@chakra-ui/react";
 import { RiInstagramLine } from "react-icons/ri";
-import { IoLogoTiktok } from "react-icons/io5";
+import { IoLogoTiktok, IoLogoInstagram, IoLogoYoutube } from "react-icons/io5";
+import CreatedBy from "./CreatedBy";
 
 export default function Footer() {
   const { asPath } = useRouter();
@@ -11,8 +21,13 @@ export default function Footer() {
 
   const socialLinks = [
     {
+      title: "Youtube",
+      icon: IoLogoYoutube,
+      url: "https://www.youtube.com/c/liamotta",
+    },
+    {
       title: "Instagram",
-      icon: RiInstagramLine,
+      icon: IoLogoInstagram,
       url: "https://www.instagram.com/sorrialia/",
     },
     {
@@ -22,5 +37,29 @@ export default function Footer() {
     },
   ];
 
-  return <></>;
+  return (
+    <>
+      <Box margin={isHome ? "90vh 0 0 0" : "100px 0 0 0"} py="50px">
+        <Center>
+          {socialLinks.map((sn) => (
+            <ChakraLink
+              borderRadius="100%"
+              key={sn.title}
+              mx="30px"
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+              w="70px"
+              h="70px"
+              href={sn.url}
+              isExternal
+            >
+              <Icon as={sn.icon} fontSize="35px" color="gray.900" />
+            </ChakraLink>
+          ))}
+        </Center>
+      </Box>
+      <CreatedBy />
+    </>
+  );
 }
